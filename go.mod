1b9bc8010443509f5cd302b5110570b2a8c99e61
Murat Tunc <murat.tunc8558@gmail.com>
2024-11-13 12:27:39 +0300

    Added lingua detect model

--- go.mod
+++ go.mod
@@ -4,8 +4,12 @@ go 1.20
 
 require (
 	github.com/bregydoc/gtranslate v0.0.0-20200913051839-1bd07f6c1fc5 // indirect
+	github.com/pemistahl/lingua-go v1.4.0 // indirect
 	github.com/robertkrimen/otto v0.2.1 // indirect
 	github.com/rs/cors v1.9.0 // indirect
+	github.com/shopspring/decimal v1.3.1 // indirect
+	golang.org/x/exp v0.0.0-20221106115401-f9659909a136 // indirect
 	golang.org/x/text v0.11.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/sourcemap.v1 v1.0.5 // indirect
 )
--- main.go
+++ main.go
@@ -7,8 +7,91 @@ import (
 	"net/http"
 
 	"github.com/bregydoc/gtranslate"
+	"github.com/pemistahl/lingua-go"
 	"github.com/rs/cors"
 )
+
+// Language represents a language with a two-letter code and name.
+type Language struct {
+	Code string // e.g., "en" for English
+	Name string // e.g., "English"
+}
+
+// Languages is a slice of available languages
+var Languages = []Language{
+	{Code: "af", Name: "Afrikaans"},
+	{Code: "sq", Name: "Albanian"},
+	{Code: "ar", Name: "Arabic"},
+	{Code: "hy", Name: "Armenian"},
+	{Code: "bn", Name: "Bengali"},
+	{Code: "bs", Name: "Bosnian"},
+	{Code: "ca", Name: "Catalan"},
+	{Code: "hr", Name: "Croatian"},
+	{Code: "cs", Name: "Czech"},
+	{Code: "da", Name: "Danish"},
+	{Code: "nl", Name: "Dutch"},
+	{Code: "en", Name: "English"},
+	{Code: "eo", Name: "Esperanto"},
+	{Code: "et", Name: "Estonian"},
+	{Code: "tl", Name: "Filipino"},
+	{Code: "fi", Name: "Finnish"},
+	{Code: "fr", Name: "French"},
+	{Code: "de", Name: "German"},
+	{Code: "el", Name: "Greek"},
+	{Code: "gu", Name: "Gujarati"},
+	{Code: "hi", Name: "Hindi"},
+	{Code: "hu", Name: "Hungarian"},
+	{Code: "is", Name: "Icelandic"},
+	{Code: "id", Name: "Indonesian"},
+	{Code: "it", Name: "Italian"},
+	{Code: "ja", Name: "Japanese"},
+	{Code: "jw", Name: "Javanese"},
+	{Code: "km", Name: "Khmer"},
+	{Code: "ko", Name: "Korean"},
+	{Code: "la", Name: "Latin"},
+	{Code: "lv", Name: "Latvian"},
+	{Code: "lt", Name: "Lithuanian"},
+	{Code: "mk", Name: "Macedonian"},
+	{Code: "ml", Name: "Malayalam"},
+	{Code: "mr", Name: "Marathi"},
+	{Code: "my", Name: "Myanmar (Burmese)"},
+	{Code: "ne", Name: "Nepali"},
+	{Code: "no", Name: "Norwegian"},
+	{Code: "pl", Name: "Polish"},
+	{Code: "pt", Name: "Portuguese"},
+	{Code: "pa", Name: "Punjabi"},
+	{Code: "ro", Name: "Romanian"},
+	{Code: "ru", Name: "Russian"},
+	{Code: "sr", Name: "Serbian"},
+	{Code: "si", Name: "Sinhala"},
+	{Code: "sk", Name: "Slovak"},
+	{Code: "sl", Name: "Slovenian"},
+	{Code: "es", Name: "Spanish"},
+	{Code: "su", Name: "Sundanese"},
+	{Code: "sw", Name: "Swahili"},
+	{Code: "sv", Name: "Swedish"},
+	{Code: "ta", Name: "Tamil"},
+	{Code: "te", Name: "Telugu"},
+	{Code: "th", Name: "Thai"},
+	{Code: "tr", Name: "Turkish"},
+	{Code: "uk", Name: "Ukrainian"},
+	{Code: "ur", Name: "Urdu"},
+	{Code: "vi", Name: "Vietnamese"},
+	{Code: "cy", Name: "Welsh"},
+	{Code: "xh", Name: "Xhosa"},
+	{Code: "yi", Name: "Yiddish"},
+	{Code: "zu", Name: "Zulu"},
+}
+
+// GetLanguageCode returns the two-letter code for a given language name
+func GetLanguageCode(languageName string) (string, bool) {
+	for _, lang := range Languages {
+		if lang.Name == languageName {
+			return lang.Code, true
+		}
+	}
+	return "", false
+}
 
 type TranslateRequest struct {
 	Text string `json:"text"`
@@ -52,6 +135,45 @@ func TranslateHandler(w http.ResponseWriter, r *http.Request) {
 	sendJSONResponse(w, response, http.StatusOK)
 }
 
+func GetLanguageHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var request TranslateRequest
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		sendErrorResponse(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	var code string
+	detector := lingua.NewLanguageDetectorBuilder().
+		FromAllLanguages().
+		WithLowAccuracyMode().
+		Build()
+
+	if detectedLang, exists := detector.DetectLanguageOf(request.Text); exists {
+		fmt.Println(detectedLang)
+		langName := detectedLang.String()
+		if code, found := GetLanguageCode(langName); found {
+			fmt.Println("Detected language code:", code)
+		} else {
+			fmt.Println("Language not found in code map.")
+			sendErrorResponse(w, "Language Detection is failed", http.StatusInternalServerError)
+		}
+	}
+
+	response := TranslateResponse{
+		TranslatedText: "Detectlanguage",
+		Status:         true,
+		Message:        code,
+	}
+
+	sendJSONResponse(w, response, http.StatusOK)
+}
+
 func sendErrorResponse(w http.ResponseWriter, message string, statusCode int) {
 	response := TranslateResponse{
 		Status:  false,
@@ -72,6 +194,7 @@ func sendJSONResponse(w http.ResponseWriter, data interface{}, statusCode int) {
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/translate", TranslateHandler)
+	mux.HandleFunc("/getLanguageCode", GetLanguageHandler)
 
 	c := cors.Default().Handler(mux)
 
